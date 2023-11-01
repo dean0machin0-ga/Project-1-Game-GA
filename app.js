904dbff79bc2bbf3707de1011efdbff1b93c4ee1
dean0machin0 <dean0machin00@gmail.com>
2023-11-01 14:58:38 -0700

    JS, css, html Update

--- app.js
+++ app.js
@@ -3,62 +3,46 @@ console.log('js-loaded!')
 
 /*----- state variables -----*/
 
-const formLocation = document.querySelector('#guess-form');  
-const letterBtnEl = document.querySelectorAll("#controller");
+// const letterBtnEl = document.querySelectorAll("#controller");
+const formLocation = document.querySelector('.form'); 
 const solutionEl = document.querySelectorAll("#solution-display");
 const gameRestartEl = document.querySelector("#rocket-message");
-const wordToGuess = ["B", "E", "S", "t"]
+const solutionDisplayEl = document.querySelector('#solution-display');
+const wordToGuess = ["B", "E", "S", "T"]
 const spaces = ["_", "_", "_", "_"]
 
 /*----- event listeners -----*/
 
-document.addEventListener('DOMContentLoaded', function () {
-    const solutionDisplay = document.querySelector('#solution-display');
-        
-    for (let i = 0; i < wordToGuess.length; i++) {
-        const underscore = document.createElement('h1')
-        underscore.setAttribute('class', 'underscore')
-        underscore.innerText = "?"
-        solutionDisplay.append(underscore)
-    }
-})
+for (let i = 0; i < wordToGuess.length; i++) {
+    const underscore = document.createElement('h1')
+    underscore.setAttribute('class', 'underscore')
+    underscore.innerText = "?"
+    // document.body.append(underscore)
+    solutionDisplayEl.append(underscore)
+}
 
-formLocation.addEventListener('input', (e) => {
+
+formLocation.addEventListener('submit', (e) => {
     e.preventDefault()
-    const userInput = document.querySelector('#guess-input').value
-    
-    if (wordToGuess.includes(userInput.toUpperCase())) {
-        
-        const foundIdx = wordToGuess.indexOf(userInput.toUpperCase())
+    const userInput = document.querySelector('#userinput').value
+    // look at how to join an array turns it into a string 
+    if (wordToGuess.join(userInput.toUpperCase())) {
+        const foundIdx = wordToGuess.includes(userInput.toUpperCase())
         console.log(foundIdx)
     } else {
-        console.log("Try again!")
-    }
+        console.log("Try again!")                              
+    } 
 })
 
-// console.log('hiii')
 
 // OLD CODE
 
-
-// document.addEventListener('DOMContentLoaded', function () {
-// })
-    
-    // const checkArr = (userInput) => {
-    //     if (wordToGuess.includes(userInput.toUpperCase())) {
-    //         //   console.log("True")
-    //         const index = 
-    //         wordToGuess.indexOf(userInput.toUpperCase())
-    //         //   console.log(index)
-    //         spaces.splice(1, 1, userInput.toUpperCase())
-    //     }
-    // }
-
-    
-    
-    // checkArr(event.currentTarget.input.value)
-    
-    
-
-
-/*----- functions -----*/
+// checkArr(event.currentTarget.input.value)
+// const checkArr = (userInput) => {
+//     if (wordToGuess.includes(userInput.toUpperCase())) {
+//         // console.log("True")
+// const index = wordToGuess.indexOf(userInput.toUpperCase())
+//         // console.log(index)
+//         spaces.splice(1, 1, userInput.toUpperCase())
+//     }
+// }
