4fa0e765ee4f4fefe037327c7384b7814b931281
dean0machin0 <dean0machin00@gmail.com>
2023-11-01 15:37:35 -0700

    MVP

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-console.log('js-loaded!')
-
-
-/*----- state variables -----*/
-
-// const letterBtnEl = document.querySelectorAll("#controller");
 const formLocation = document.querySelector('.form'); 
 const solutionEl = document.querySelectorAll("#solution-display");
 const gameRestartEl = document.querySelector("#rocket-message");
@@ -11,38 +5,21 @@ const solutionDisplayEl = document.querySelector('#solution-display');
 const wordToGuess = ["B", "E", "S", "T"]
 const spaces = ["_", "_", "_", "_"]
 
-/*----- event listeners -----*/
-
 for (let i = 0; i < wordToGuess.length; i++) {
     const underscore = document.createElement('h1')
     underscore.setAttribute('class', 'underscore')
     underscore.innerText = "?"
-    // document.body.append(underscore)
     solutionDisplayEl.append(underscore)
 }
 
-
 formLocation.addEventListener('submit', (e) => {
     e.preventDefault()
-    const userInput = document.querySelector('#userinput').value
-    // look at how to join an array turns it into a string 
-    if (wordToGuess.join(userInput.toUpperCase())) {
-        const foundIdx = wordToGuess.includes(userInput.toUpperCase())
-        console.log(foundIdx)
+    let userInput = document.querySelector('#userinput')
+    if (wordToGuess.join('') === userInput.value.toUpperCase()) {
+        console.log('correct!!')
+        userInput.value = ''
     } else {
-        console.log("Try again!")                              
+        console.log("Try again!")
+        userInput.value = ''                              
     } 
 })
-
-
-// OLD CODE
-
-// checkArr(event.currentTarget.input.value)
-// const checkArr = (userInput) => {
-//     if (wordToGuess.includes(userInput.toUpperCase())) {
-//         // console.log("True")
-// const index = wordToGuess.indexOf(userInput.toUpperCase())
-//         // console.log(index)
-//         spaces.splice(1, 1, userInput.toUpperCase())
-//     }
-// }
