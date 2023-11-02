24518e8d60320c38726f8ef128d6d11a29f63451
dean0machin0 <dean0machin00@gmail.com>
2023-11-01 18:27:01 -0700

    MVP

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const formLocation = document.querySelector('.form'); 
-const solutionEl = document.querySelectorAll("#solution-display");
-const gameRestartEl = document.querySelector("#rocket-message");
+const solutionEl = document.querySelectorAll('#solution-display');
+const gameRestartEl = document.querySelector('#rocket-message');
 const solutionDisplayEl = document.querySelector('#solution-display');
-const wordToGuess = ["B", "E", "S", "T"]
-const spaces = ["_", "_", "_", "_"]
+// const wordToGuess = ['B', 'E', 'S', 'T']
+const wordToGuess = 'BEST';
+const spaces = '_'.repeat(wordToGuess.length);
+const letterEl = document.createElement('h1');
+        letterEl.setAttribute('class', 'underscore');
+        letterEl.innerText = spaces
+        solutionDisplayEl.append(letterEl)
 
-for (let i = 0; i < wordToGuess.length; i++) {
-    const underscore = document.createElement('h1')
-    underscore.setAttribute('class', 'underscore')
-    underscore.innerText = "?"
-    solutionDisplayEl.append(underscore)
-}
+
+// for (let i = 0; i < wordToGuess.length; i++) {
+//     const underscore = document.createElement('h1')
+//     underscore.setAttribute('class', 'underscore')
+// underscore.innerText = '?'
+//     solutionDisplayEl.append(underscore)
+// }
 
 formLocation.addEventListener('submit', (e) => {
     e.preventDefault()
     let userInput = document.querySelector('#userinput')
-    if (wordToGuess.join('') === userInput.value.toUpperCase()) {
+    if (wordToGuess === userInput.value.toUpperCase()) {
         console.log('correct!!')
+        document.querySelector('.underscore').innerText = wordToGuess
         userInput.value = ''
     } else {
-        console.log("Try again!")
+        console.log('Try again!')
         userInput.value = ''                              
     } 
 })
